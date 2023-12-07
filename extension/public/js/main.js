9f51e0547a25589952c1a01d2f49000abaff8bca
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2023-12-07 14:36:51 +0530

    Get tweet on click

--- extension/public/js/main.js
+++ extension/public/js/main.js
@@ -2,4 +2,6 @@ console.log('main.js');
 
 document.querySelector('[data-check="true"]').parentElement.onclick = function() {
   console.log('clicked');
+  let tweetText = document.querySelector('.DraftEditor-editorContainer').innerText;
+  // taken tweet text + wallet address -> send to server?
 }
